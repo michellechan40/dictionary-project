eb54320cd9bcfad18c7205482c4e692fbba73eb7
michellechan40 <102484481+michellechan40@users.noreply.github.com>
2022-10-12 18:12:41 -0600

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="" className="App-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
